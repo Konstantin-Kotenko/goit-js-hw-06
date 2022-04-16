33c0ae3355b0d71fefc184b25e92969be9c571f3
Konstantin-Kotenko <kostya.kotenkoo@gmail.com>
2022-04-16 22:26:39 +0300

    finish task 5

--- js/task-05.js
+++ js/task-05.js
@@ -3,5 +3,8 @@ console.log(inputEl);
 const spanEl = document.querySelector('span#name-output');
 console.log(spanEl);
 inputEl.addEventListener('input', (event) => {
-  spanEl.textContent = event.currentTarget.value;
+  if (event.currentTarget.value === '') {
+    return (spanEl.textContent = 'Anonymous');
+  }
+  return (spanEl.textContent = event.currentTarget.value);
 });
