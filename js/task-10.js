8d8c9516ebc8f5cf3149a661d2c3467d18d7283e
Konstantin-Kotenko <kostya.kotenkoo@gmail.com>
2022-04-17 23:13:23 +0300

    finish 10 task

--- js/task-10.js
+++ js/task-10.js
@@ -1,48 +1,39 @@
-// Задание 10 (выполнять не обязательно)
-// Напиши скрипт создания и очистки коллекции элементов. Пользователь вводит количество элементов в input и нажимает кнопку Создать, после чего рендерится коллекция. При нажатии на кнопку Очистить, коллекция элементов очищается.
-
-// <div id="controls">
-//   <input type="number" min="1" max="100" step="1" />
-//   <button type="button" data-create>Create</button>
-//   <button type="button" data-destroy>Destroy</button>
-// </div>
-
-// <div id="boxes"></div>
-// Создай функцию createBoxes(amount), которая принимает один параметр - число. Функция создает столько <div>, сколько указано в amount и добавляет их в div#boxes.
-
-// Размеры самого первого <div> - 30px на 30px.
-// Каждый элемент после первого, должен быть шире и выше предыдущего на 10px.
-// Все элементы должены иметь случайный цвет фона в формате HEX. Используй готовую функцию getRandomHexColor для получения цвета.
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215)
-//     .toString(16)
-//     .padStart(6, 0)}`;
-// }
-// Создай функцию destroyBoxes(), которая очищает содержимое div#boxes, тем самым удаляя все созданные элементы.
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 const refs = {
   input: document.querySelector('input'),
   btnCreate: document.querySelector('[data-create]'),
   btnDestroy: document.querySelector('[data-destroy]'),
+  divBoxes: document.querySelector('#boxes'),
 };
-console.log(refs.input);
-console.log(refs.btnCreate);
-console.log(refs.btnDestroy);
+let width = 30;
+let height = 30;
+function createBoxes(amount) {
+  const boxes = [];
+  for (let i = 0; i < amount; i += 1) {
+    const box = document.createElement('div');
+    box.style.width = width + 'px';
+    box.style.height = height + 'px';
+    box.style.backgroundColor = getRandomHexColor();
+    width += 10;
+    height += 10;
+    console.log(box);
+    boxes.push(box);
+  }
+  return refs.divBoxes.append(...boxes);
+}
+const destroyBoxes = () => {
+  const boxes = refs.divBoxes.querySelectorAll('div');
+  boxes.forEach((box) => box.remove());
+  refs.input.value = null;
+};
+
 refs.input.addEventListener('input', onInput);
 function onInput(event) {
   event.currentTarget.value = refs.input.value;
-  console.log(event.currentTarget.value);
 }
 
-refs.btnCreate.addEventListener('click', onBtnCreateClick);
-function onBtnCreateClick(event) {
-  console.log('click');
-}
-refs.btnDestroy.addEventListener('click', onBtnDestroyClick);
-function onBtnDestroyClick(event) {
-  console.log('-click');
-}
-function createBoxes(amount) {}
-console.log(createBoxes);
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+refs.btnCreate.addEventListener('click', () => createBoxes(refs.input.value));
+
+refs.btnDestroy.addEventListener('click', destroyBoxes);
